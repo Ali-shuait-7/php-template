1f71af2cbce3e93043726ec610d92fa647775b96
Ali Shuait <Ali.Shuait@hotmail.com>
2026-01-21 13:03:56 -0800

    Update database configuration with placeholders

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    "driver" => "mysql",
-    "host" => "localhost",
-    "user" => "root",
-    "password" => "root",
-    "name" => "clinic_booking_sys",
+    "driver" => "your_driver" ?? "mysql",
+    "host" => "your_hostname"??"localhost",
+    "user" => "your_db_username",
+    "password" => "your_db_password",
+    "name" => "database_name",
     "charset" => "utf8mb4"
 ];
